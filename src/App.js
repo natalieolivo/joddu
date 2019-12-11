9331de8662d4cee7903d4842fb379a3c02c48b3a
NATALIE OLIVO <natalieolivo@NATALIEsMacBook.fios-router.home>
2019-12-10 21:39:46 -0500

    POC for homepage
    * Nav styling treatment
    * Menu positioning
    * Styling for Grid and Grid Blocks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,163 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from './components/Menu';
 import './App.css';
+import styled from 'styled-components';
+
+const API_ENDPOINT = "";
+const Header = styled.header`  
+  display: block;
+  width: 100%;
+  border-bottom: solid 1px #ddd;
+  margin-bottom: .8em;
+`;
+
+const GridHeader = styled.header`
+  
+`;
+
+const GridFilter = styled.section`
+    display: flex;
+    flex-flow: row;    
+`;
+
+const GridFilterBlock = styled.div`
+  border-radius: 5em;
+`;
+
+const Grid = styled.section`    
+  display: flex;
+  flex-direction: row;
+  flex-flow: wrap;
+`;
+
+const GridBlock = styled.div`    
+  width: 252px;
+`;
+
+const GridWrapper = styled.div`  
+`;
+
+const GridImg = styled.img`
+  border-radius: 16px;
+`;
+
+const SearchInput = styled.input`  
+  height: 2em;
+  margin: .8em;
+  font-size: 28px;
+  border: solid 1px #ddd;  
+`;
+
+const SearchBtn = styled.button`
+  margin: .8em;
+  font-size: 28px;
+  padding: .6em;
+  cursor: pointer;
+  background: #ddd;
+  border: none;
+  outline: 0;
+`;
+
+// Calls api to return types of hair 
+// sample schema above 
+/* {
+    type: "locks",
+    location: "jamaica, ny",
+    zipcode: "11435"
+    image : []
+    pricing: []
+}, {
+  type: braids,
+  location: "jamaica, ny",
+  zipcode: "11435",
+  image: [],
+  pricing: []
+}
+*/
 
 function App() {
+  const [styles] = useState([{
+    style_type: "locks",
+    location: "jamaica, ny",
+    zipcode: "11435",
+    image : {
+      src: "https://i.pinimg.com/236x/f0/6e/33/f06e33bbe7cec830ea26b95b61a9dbe0.jpg"
+    },
+    pricing: []
+  }, {
+    style_type: "locks",
+    location: "jamaica, ny",
+    zipcode: "11435",
+    image: {
+      src: "https://i.pinimg.com/236x/78/17/fe/7817fe214f7fabaab1970c3eefddb533.jpg"
+    },
+    pricing: []
+  }, {
+    style_type: "locks",
+    location: "jamaica, ny",
+    zipcode: "11435",
+    image: {
+      src: "https://i.pinimg.com/236x/5c/5e/8b/5c5e8bd39e0dac6f298d7fe6e17a0685.jpg"
+    },
+    pricing: []
+  }, {
+    style_type: "locks",
+    location: "jamaica, ny",
+    zipcode: "11435",
+    image: {
+      src: "https://i.pinimg.com/236x/b6/3b/3a/b63b3a58f22b42537241414fbef74841.jpg"
+    },
+    pricing: []
+  }]);
+
+  const [filters] = useState([{
+    label: "braided",
+    url: ""
+  }, {
+    label: "natural",
+    url: ""
+  }]);
+
+  useEffect(() => {
+  
+    // fetch(API_ENDPOINT)
+    //   .then(response => response.json())
+    //   .then(json => console.log(json));
+  
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <Menu />
-      </header>
+      </Header>
+
+      <span>Find and book a stylist:</span>
+      <SearchInput type="text" />
+      <SearchBtn>search</SearchBtn>
+      <a href="">Stylists click here</a>
+
+      <GridHeader>Hair Styles</GridHeader>
+      <GridFilter>
+        {
+          filters.map(filter => {
+            return <GridFilterBlock>{filter.label}</GridFilterBlock>
+          })
+        }
+      </GridFilter>
+
+      <Grid>
+        {styles.map(({type, image}) => {      
+          return (
+            <GridBlock>
+              <GridWrapper>
+                <GridImg alt="" src={image.src} />
+                {type}
+              </GridWrapper>
+            </GridBlock>
+          )
+        })}
+      </Grid>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import MenuButton from '../svgs/MenuButton';
+
+const Nav = styled.nav`
+    font-size: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const MenuArrowSVG = styled.svg`
+    position: absolute;
+    top: -16px;
+    left: 15px;
+    fill: #fff;
+    background: #fff;
+    z-index: 1;
+`;
+
+const List = styled.section`
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    top: 4.7em;
+    left: .4em;
+    right: .4em;
+    color: #000;    
+    background: #fff;
+    border: solid 1px #ddd;    
+    border-radius: 8px;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, .2);    
+`;
+
+const ListItems = styled.ul`
+    list-style: none;
+
+    &.li {
+        
+    }
+`;
+
+const Title = styled.h2`
+    color: #000;
+    font-family: Futura;    
+`;
+
+const CartButton = styled.section`
+    margin: .9em .9em 0 auto;    
+`;
 
 function Menu() {
-    return (
-        <navigation>
-            <ul>
-                <li>menu item 1</li>
-                <li>menu item 2</li>
-            </ul>
-        </navigation>
+    let [menuVisible, setMenuState] = useState(false);
+
+    const menuDisplay = () => {        
+        if(menuVisible) {
+            return (
+                <List>
+                    <MenuArrowSVG version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20">
+                        <path fill="#ddd" d="M0 15c0 0.128 0.049 0.256 0.146 0.354 0.195 0.195 0.512 0.195 0.707 0l8.646-8.646 8.646 8.646c0.195 0.195 0.512 0.195 0.707 0s0.195-0.512 0-0.707l-9-9c-0.195-0.195-0.512-0.195-0.707 0l-9 9c-0.098 0.098-0.146 0.226-0.146 0.354z"></path>
+                    </MenuArrowSVG>                    
+                    <ListItems>
+                        <li>Home</li>
+                        <li>Appointments</li>
+                        <li>Logout</li>
+                    </ListItems>
+                </List>
+            )
+        }    
+    }
+    
+    const setMenuDisplay = () => {
+        setMenuState(!menuVisible);
+    }
+
+    return (              
+        <Nav>
+            <MenuButton setMenuDisplay={setMenuDisplay} />
+            {menuDisplay()}
+            <Title>Hair Care App</Title>
+            <CartButton>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 16 16">
+                    <path fill="#000000" d="M6 14.5c0 0.828-0.672 1.5-1.5 1.5s-1.5-0.672-1.5-1.5c0-0.828 0.672-1.5 1.5-1.5s1.5 0.672 1.5 1.5z"></path>
+                    <path fill="#000000" d="M16 14.5c0 0.828-0.672 1.5-1.5 1.5s-1.5-0.672-1.5-1.5c0-0.828 0.672-1.5 1.5-1.5s1.5 0.672 1.5 1.5z"></path>
+                    <path fill="#000000" d="M16 8v-6h-12c0-0.552-0.448-1-1-1h-3v1h2l0.751 6.438c-0.458 0.367-0.751 0.93-0.751 1.562 0 1.105 0.895 2 2 2h12v-1h-12c-0.552 0-1-0.448-1-1 0-0.003 0-0.007 0-0.010l13-1.99z"></path>
+                </svg>                
+            </CartButton>                      
+        </Nav>        
     )
 }
 
