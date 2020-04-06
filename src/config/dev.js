b57f38fb705872a9c66e4536a1e8a62c2392c9e8
NATALIE OLIVO <natalieolivo@nataliesmacbook.lan>
2020-04-05 20:58:58 -0400

    Remove https

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,6 @@
 export const config = {
-  AUTH_SIGNIN_ENDPOINT: `https://joddu-api.herokuapp.com/api/signin`,
-  AUTH_SIGNUP_ENDPOINT: `https://joddu-api.herokuapp.com/api/signup`,
-  AUTH_SIGNOUT_ENDPOINT: `https://joddu-api.herokuapp.com/api/signout`,
-  API_REGISTER_ENDPOINT: `https://joddu-api.herokuapp.com/api/stylist`
+  AUTH_SIGNIN_ENDPOINT: `http://joddu-api.herokuapp.com/api/signin`,
+  AUTH_SIGNUP_ENDPOINT: `http://joddu-api.herokuapp.com/api/signup`,
+  AUTH_SIGNOUT_ENDPOINT: `http://joddu-api.herokuapp.com/api/signout`,
+  API_REGISTER_ENDPOINT: `http://joddu-api.herokuapp.com/api/stylist`
 };
